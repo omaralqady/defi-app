c5c95d824dcdc6382f21b0072d936734270b1dcd
Omar Alqady <omar@founda.com>
2021-10-31 19:05:15 +0100

    test: get token value

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -1,12 +1,21 @@
 from scripts.deploy import deploy_token_and_token_farm
 from brownie import network, exceptions
-from scripts.utils import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account, get_contract
+from scripts.utils import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    get_contract,
+    INIT_PRICE_VALUE,
+)
 import pytest
 
 
-def test_set_price_feed_contract():
+def check_network():
     if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         pytest.skip("Only for local tests")
+
+
+def test_set_price_feed_contract():
+    check_network()
     account = get_account()
     farm, token = deploy_token_and_token_farm()
     price_feed = get_contract("eth_usd_price_feed")
@@ -16,8 +25,7 @@ def test_set_price_feed_contract():
 
 
 def test_set_price_feed_contract_fails_with_non_owner():
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip("Only for local tests")
+    check_network()
     non_owner = get_account(index=1)
     farm, token = deploy_token_and_token_farm()
     price_feed = get_contract("eth_usd_price_feed")
@@ -27,9 +35,7 @@ def test_set_price_feed_contract_fails_with_non_owner():
 
 
 def test_allow_token():
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip("Only for local tests")
-    account = get_account()
+    check_network()
     farm, token = deploy_token_and_token_farm()
 
     farm.addAllowedToken(token.address)
@@ -38,8 +44,7 @@ def test_allow_token():
 
 
 def test_stake_tokens(amount_staked):
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip("Only for local tests")
+    check_network()
     account = get_account()
     farm, token = deploy_token_and_token_farm()
 
@@ -49,3 +54,18 @@ def test_stake_tokens(amount_staked):
     farm.stakeTokens(amount_staked, token.address, {"from": account})
 
     assert farm.stakingBalance(token.address, account.address) == amount_staked
+    assert farm.stakers(0) == account.address
+    assert farm.uniqueTokensStaked(account.address) == 1
+
+
+def test_get_token_value():
+    check_network()
+    account = get_account()
+    farm, token = deploy_token_and_token_farm()
+
+    price_feed = get_contract("eth_usd_price_feed")
+    farm.setPriceFeedContract(token.address, price_feed, {"from": account})
+
+    (price, decimal) = farm.getTokenValue(token.address)
+
+    assert price == INIT_PRICE_VALUE
