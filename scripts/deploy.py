23edb99c966635bf4a321d92ef7435e7991a0d39
Omar Alqady <omar@founda.com>
2021-11-07 21:50:50 +0100

    feat: add update front end script

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,6 +1,10 @@
 from scripts.utils import get_account, get_contract
 from brownie import FPSToken, TokenFarm, network, config
 from web3 import Web3
+import yaml
+import json
+import os
+import shutil
 
 
 KEPT_BALANCE = Web3.toWei(10, "ether")
@@ -46,10 +50,35 @@ def add_allowed_tokens(token_farm, token_list, account):
         tx.wait(1)
 
 
-def deploy_token_and_token_farm():
+def deploy_token_and_token_farm(update_fe=False):
     token = deploy_token()
     farm = deploy_token_farm(token.address)
+    if update_fe:
+        update_front_end()
     return farm, token
+
+
+def update_front_end():
+    ### This is only because this is a test scenario with multiple deployments
+    ### In real life scenarios, once the contracts are deployed, the address
+    ### is set and would be configured in the app with no need to update
+    ### the config upon each run of the script
+
+    # copy the build folder to the fe so that it can access our token's ABI
+    copy_folders_to_front_end("./build", "./front_end/src/chain-info")
+
+    # copy brownie config to fe so that fe can access contract addresses
+    with open("brownie-config.yaml", "r") as brownie_config:
+        config_values = yaml.load(brownie_config, Loader=yaml.FullLoader)
+        with open("./front_end/src/brownie-config.json", "w") as brownie_config_json:
+            json.dump(config_values, brownie_config_json)
+            print("Front end updated!")
+
+
+def copy_folders_to_front_end(src, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(src, dest)
 
 
 def main():
@@ -63,6 +92,6 @@ def main():
         "fps_token": get_contract("dai_usd_price_feed"),
     }
 
-    farm, token = deploy_token_and_token_farm()
+    farm, token = deploy_token_and_token_farm(update_fe=True)
     send_tokens_to_farm(token, farm, token_list)
     add_allowed_tokens(farm, token_list, get_account())
